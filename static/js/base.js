023acfa3a7d447aa7bbb444f09fecd500a082814
Lingvistik <emil1811@gmail.com>
2020-09-09 23:55:08 +0000

    javascript code added, homepage structure set

---
+++ static/js/base.js
@@ -0,0 +1,60 @@
+Array.prototype.shuffle = function() {
+        var s = [];
+        while (this.length) s.push(this.splice(Math.random() * this.length, 1));
+        while (s.length) this.push(s.pop());
+        return this;}
+
+var picPaths = [
+                'media/elguitar1.jpg',
+                'media/acguitar1.jpg',
+                'media/flute.jpg',
+                'media/keyboard.jpg',
+                'media/sax.jpg',
+                'media/acguitar6.jpg',
+                'media/piano6.jpg',
+                'media/marimba.jpg',
+                ];
+
+picPaths.shuffle();
+
+picO = new Array();
+
+for(i=0; i < picPaths.length; i++){
+    picO[i] = new Image();
+    picO[i].src = picPaths[i];
+    }
+
+window.onload=function(){
+                
+var mainImgs = document.getElementById('home-feature-container').getElementsByTagName('img');
+    for(i=0; i < mainImgs.length; i++){
+        mainImgs[i].src = picO[i].src;
+        }
+    }
+
+
+/*                
+var features = new Array('media/acguitar1.jpg','media/acguitar6.jpg','media/elguitar1.jpg','media/keyboard.jpg','media/piano6.jpg')
+
+function choosePic1() {
+     var randomNum = Math.floor(Math.random() * features.length);
+     document.getElementById("feature1").src = features[randomNum];
+    }
+
+function choosePic2() {
+     var randomNum = Math.floor(Math.random() * features.length);
+     document.getElementById("feature2").src = features[randomNum];
+    }
+
+function choosePic3() {
+     var randomNum = Math.floor(Math.random() * features.length);
+     document.getElementById("feature3").src = features[randomNum];
+    }
+
+window.onload = function() {
+    var img1 = choosePic1() ;
+    var img2 = choosePic2() ;
+    var img3 = choosePic3() ;
+}
+
+*/
