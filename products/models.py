24ace1082c7d7537e1ef1fa8e70e94cf5ae8b709
Lingvistik <emil1811@gmail.com>
2020-09-17 22:27:25 +0000

    Product structure and view sorted end edited, product model and fixture updated

--- products/models.py
+++ products/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    description2 = models.TextField(default="default")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
