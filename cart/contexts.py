d6df0d1a2ddf975830646b20ca56cdd8cde3f805
Lingvistik <emil1811@gmail.com>
2020-09-20 00:34:35 +0000

    Shopping cart page added, items can be added to the cart

---
+++ cart/contexts.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_content(request):
+
+    cart_items = []
+    total = 0
+    item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        item = get_object_or_404(Product, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+    
+    grand_total = delivery + total
+    
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'item_count': item_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+
+    return context
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def view_cart(request):
+    """ A view to show the content of the shopping cart """
+    
+    return render(request, 'cart/shopping-cart.html')
+
+def add_to_cart(request, item_id):
+
+    """ Functionality to add item quantity to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
