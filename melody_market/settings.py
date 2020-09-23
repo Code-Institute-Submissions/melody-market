88e5c5e9b138a6a3e03e1bc9ae2af08482ce9760
Lingvistik <emil1811@gmail.com>
2020-09-23 00:10:59 +0000

    data loaded to Heroku database

--- melody_market/settings.py
+++ melody_market/settings.py
@@ -117,17 +117,17 @@ WSGI_APPLICATION = 'melody_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else: 
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse('postgres://ajwtemudnuktjb:3caf16c1debab858cc834e3d6626ed89fe65b3291170b92afcb1f9d4144cb423@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/da7os5g7viecd7')
+}
+#else: 
+#   DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
